b696cef0c26792a0c76ea712301ee1299e442659
Sen-Zhang <senzhang1987@126.com>
2015-01-28 15:56:41 -0800

    init an app with grunt

---
+++ grunt/angular-app/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name learnAngularApp
+ * @description
+ * # learnAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('learnAngularApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
